c29ec88125f68f6b32724836ce3a3dac0da764ec
lisaxyang <66920418+lisaxyang@users.noreply.github.com>
2020-10-30 16:40:38 -0400

    finished studio

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?token=XinQDHAn1nW30iWHV0eycQwKCmRwWEYGG7CVdXmR57Q";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -20,12 +20,62 @@ const corsPromise = () =>
     resolve(request);
   });
 
+
+// parse data and render HTML
+const getData = (response) => {
+  const plantData = JSON.parse(response).data;
+  console.log(plantData);
+  const beechData = plantData.filter(item => item.family_common_name == "Beech family");
+  // console.log(plantData[0]);
+  // console.log(plantData[0].common_name);
+  var i;
+  for (i = 0; i < beechData.length; i++) {
+    addToDom(beechData[i], "beechplants");
+  }
+  // addToDom(plantData[0]);
+
+
+  const buttercupData = plantData.filter(item => item.family_common_name == "Buttercup family");
+  var i;
+  for (i = 0; i < buttercupData.length; i++) {
+    addToDom(buttercupData[i], "buttercupplants");
+  }
+}
+
+const addToDom = (plant, div) => {
+  const wrapperDiv = document.createElement("div");
+  wrapperDiv.setAttribute("class", "plant.common_name");
+
+  const plant_name = document.createElement("h3");
+  plant_name.innerText = plant.common_name;
+
+  const image_url = plant.image_url;
+  const plant_img = document.createElement("img");
+  plant_img.setAttribute("src", image_url);
+
+  wrapperDiv.appendChild(plant_name);
+  wrapperDiv.appendChild(plant_img);
+
+  document.getElementById(div).appendChild(wrapperDiv);
+}
+
+// function justBeech(plant) {
+//   plant.family_common_name == "Beech family";
+// }
+
+// function filterBeech() {
+//   document.getElementById("plantData").innerHTML = plants.filter(justBeech);
+// }
+
 // THIS IS SOME SAMPLE CODE FOR HOW TO USE PROMISES -- feel free to adapt this into a function!
-corsPromise().then(
-  (request) =>
-    (request.onload = request.onerror = function () {
-      // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
-    })
-);
+const displayContent = () => {
+  corsPromise().then(
+    (request) =>
+      (request.onload = request.onerror = function () {
+        // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
+        getData(request.response);
+      })
+  );
+}
 
 //// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
